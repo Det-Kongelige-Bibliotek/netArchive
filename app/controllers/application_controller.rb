568208852c6b0d5df0c61f7585b09ad55b03a710
Nader <nkh@kb.dk>
2018-01-16 13:21:23 +0100

    added list and show document

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
-  #layout :determine_layout
-  layout 'blacklight'
+  layout :determine_layout
+  #layout 'blacklight'
 
   protect_from_forgery with: :exception
 end
